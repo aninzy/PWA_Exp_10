e394e8f190d56c28d47f95cf021c78ce3307d2e2
Anisha <anisha.prabhu26@gmail.com>
2023-03-28 14:50:34 +0530

    first commit

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', evt =>{
+    console,log("Service worker is registere")
+})
+self.addEventListener('activate', evt =>{
+    console,log("Service worker is registere")
+})
+self.addEventListener('fetch', evt =>{
+    console,log("Service worker is registere")
+})
